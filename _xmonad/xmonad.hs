b15c4680b3ebee01a1a1246e872dffb063315e1c
Vincent Ollivier <me@vinc.cc>
2013-05-16 20:45:02 +0000

    Add border color to XMonad

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -4,6 +4,8 @@ import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
 
 main = xmonad $ defaultConfig {
+  normalBorderColor  = "#dddddd",
+  focusedBorderColor = "#eeeeee",
   layoutHook = noBorders (layoutHook defaultConfig),
   modMask    = mod4Mask,
   terminal   = "urxvt"
