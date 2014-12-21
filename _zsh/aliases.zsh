a8080815dce96c7e584b9de4ae47daf3b59a41f8
Vincent Ollivier <v@vinc.cc>
2014-12-21 18:05:43 +0100

    Replace GREP_OPTIONS by alias

--- _zsh/aliases.zsh
+++ _zsh/aliases.zsh
@@ -1,7 +1,9 @@
+alias grep="grep --color=auto"
+alias ls="ls --color=auto"
+
 alias e="$EDITOR"
 alias g="git"
 alias r="rails"
-alias l="ls $LS_OPTIONS"
 alias ll="l -l"
 alias rm="rm -i"
 alias vim="vim -p"
