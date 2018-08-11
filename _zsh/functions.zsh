0012ef41569e3a0d097363c468f3826ee4fa8c53
Vincent Ollivier <v@vinc.cc>
2018-08-11 16:00:46 +0200

    Add tts command

--- _zsh/functions.zsh
+++ _zsh/functions.zsh
@@ -12,6 +12,15 @@ txt() {
   fmt $1 | less
 }
 
+tts() {
+  flite \
+    -voice /usr/local/share/flite/cmu_us_clb.flitevox \
+    --setf int_f0_target_mean=210 \
+    --setf int_f0_target_stddev=20 \
+    --setf duration_stretch=1.0 \
+    $@
+}
+
 c() {
   echo $(( $* ))
 }
