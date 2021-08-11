4052fea068ba653821c6ae973d8116c7b8c20304
Vincent Ollivier <v@vinc.cc>
2021-08-11 07:02:34 +0200

    Update zsh aliases

--- _zsh/aliases.zsh
+++ _zsh/aliases.zsh
@@ -1,32 +1,29 @@
 # Enable ls colors on GNU
 ls --color=auto &> /dev/null && alias ls="ls --color=auto"
 
-alias grep="grep --color=auto"
-alias vim="vim -p"
-
+alias b="bundle"
 alias b="bundle"
 alias e="$EDITOR"
-alias g="git"
-alias b="bundle"
-alias r="rails"
-alias ll="ls -l"
-alias rm="rm -i"
-alias ff="nohup firefox > /dev/null"
-
-alias sys="systemctl"
-
-alias rss="newsbeuter"
-alias pdf="zathura"
-alias tool="asdf"
 alias email="mutt"
+alias ff="nohup firefox > /dev/null"
+alias g="git"
+alias grep="grep --color=auto"
+alias img="feh --auto-rotate --conversion-timeout 0.1 -d."
+alias ll="ls -l"
 alias music="cmus"
-alias video="mpv"
-
+alias pdf="zathura"
 alias please="sudo" # :)
+alias r="rails"
+alias rm="rm -i"
+alias rss="newsbeuter"
+alias sys="systemctl"
+alias tool="asdf"
+alias video="mpv"
+alias vim="vim -p"
 
 #compdef g="git"
 #compdef pkg="pacaur"
 
-alias dokku='bash $HOME/.dokku/contrib/dokku_client.sh'
+alias dokku="bash $HOME/.dokku/contrib/dokku_client.sh"
 
 hash colordiff 2>/dev/null && alias diff="colordiff"
