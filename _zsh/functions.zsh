91e022d4bfba5019aaad97554eecc6a382354848
Vincent Ollivier <v@vinc.cc>
2023-04-10 17:41:44 +0200

    Update color change

--- _zsh/functions.zsh
+++ _zsh/functions.zsh
@@ -24,6 +24,14 @@ alias c="noglob c"
 set-theme() {
   test "$1" = "light"
   sed -i "s/dark-theme=[0-9]/dark-theme=$?/g" ~/.config/gtk-3.0/settings.ini
+  sed -i "s/gruvbox-\w\+/gruvbox-$1/g" ~/.config/polybar/config.ini
+  sed -i "s/gruvbox_\w\+/gruvbox_$1/g" ~/.config/alacritty/alacritty.yml
+  sed -i "s/set background=\w\+/set background=$1/g" ~/.vimrc
+  if [[ "$1" == "light" ]]; then
+    xsetroot -solid "#fbf1c7"
+  else
+    xsetroot -solid "#282828"
+  fi
 }
 
 domain() {
